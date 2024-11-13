02f7f9c821dde9f178128c8b8336ee5bcc5f6947
Priscilla EMOND <priscie0203@gmail.com>
2024-11-13 16:12:05 +0000

    presque fini

---
+++ models/staging/raw/stg_raw__raw_gz_bing.sql
@@ -0,0 +1,25 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'raw_gz_bing') }}
+
+),
+
+renamed as (
+
+    select
+        cast(date_date as STRING) AS date_date,
+        concat(date_date,"_",campaign_key) AS pk,
+        paid_source,
+        campaign_key,
+        campgn_name AS campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression,
+        click
+
+    from source
+
+)
+
+select * from renamed
