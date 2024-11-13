fe56030667d816c4572ddffd5cf270415980c764
Priscilla EMOND <priscie0203@gmail.com>
2024-11-13 16:27:12 +0000

    Victoire!!!!!

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,5 @@
-SELECT *
- FROM {{ref('stg_raw__raw_gz_adwords')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_raw__raw_gz_bing')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_raw__raw_gz_criteo')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_raw__raw_gz_facebook')}}
+ {{
+     dbt_utils.union_relations(
+         relations=[ref('stg_raw__raw_gz_adwords'), ref('stg_raw__raw_gz_bing'), ref('stg_raw__raw_gz_criteo'), ref('stg_raw__raw_gz_facebook')]
+     )
+ }}
